ee1beab48417a4345b6b1e1a1e7ffd74e090f293
Daem Pasha <daempasha@gmail.com>
2022-09-26 00:03:24 +0100

    jest.setup.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -14,7 +14,7 @@ const customJestConfig = {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 
 }
 
